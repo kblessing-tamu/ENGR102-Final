41fa7877552740da4b9644cc31fe0dfcc1142d6a
Kyle Blessing <20blessingk@cistercianmail.net>
2021-05-01 12:38:09 -0500

    Blank Template before review

---
+++ finalReview.py
@@ -0,0 +1,61 @@
+# ENGR 102 Final Review
+# Kyle Blessing
+# https://www.w3schools.com/python/default.asp
+
+# Lists
+
+
+
+
+
+# Dictionaries 
+
+
+
+
+
+# numpy
+import numpy as np
+
+
+
+
+# matplotlib
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+# misc questions
+'''
+
+name = "I enjoy programing in Python"
+print(name[4:9])
+
+
+matrix = [[1, 2, 3, 4],
+          [4, 5, 6, 7],
+          [8, 9, 10, 11],
+          [12, 13, 14, 15]]
+for i in range(0, 4):
+    print(matrix[i][1], end=" ")
+
+
+
+def func(x):
+    return x - 1
+print(func(3) * func(5))
+
+def f1():
+    print('Hi')
+def f2():
+    print('Lo')
+f2()
+f1()
+f1()
+
+
+
+'''
